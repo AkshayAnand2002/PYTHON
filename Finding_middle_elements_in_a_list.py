98e7894bd4ac662a55feeac5eecddd33d98df68c
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-05-11 08:37:28 +0530

    Add files via upload

---
+++ Finding_middle_elements_in_a_list.py
@@ -0,0 +1,10 @@
+list = [1, 2, 3, 4, 5, 6]
+first_middle = int(len(list) / 2) - 1 # = 3 - 1 = 2
+second_middle = int(len(list) / 2)    # = 3
+print(list[first_middle])  # 3
+print(list[second_middle]) # 4
+aList = [1,2,3,4,5]
+#minus 1 because the first element is index 0
+middleIndex = int((len(aList) - 1)/2)
+print(middleIndex)
+print(aList[middleIndex])
